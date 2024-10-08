af865e65fb665388fd2486c8466f341530d40a2e
c4droid <c4droid@foxmail.com>
2024-10-08 12:42:02 +0800

    emacs: Add pinentry

--- emacs/lisp/init-gnupg.el
+++ emacs/lisp/init-gnupg.el
@@ -1,4 +1,7 @@
 ;; -*- lexical-binding: t -*-
 (setup epa
   (:option epa-pinentry-mode 'loopback))
+
+(setup (:straight-when pinentry (executable-find "gpg"))
+  (pinentry-start))
 (provide 'init-gnupg)
