d9445ce97bf9b36aeced8335109250f6dd7c6e22
c4droid <c4droid@foxmail.com>
2024-10-08 12:41:15 +0800

    emacs: Modify cnfonts load condition

--- emacs/lisp/init-cnfonts.el
+++ emacs/lisp/init-cnfonts.el
@@ -1,5 +1,5 @@
 ;; -*- lexical-binding: t -*-
-(setup (:straight cnfonts)
+(setup (:straight-when cnfonts (display-graphic-p))
   (:option cnfonts-directory (expand-file-name "cnfonts" no-littering-var-directory))
   (:hook-into after-init-hook))
 (provide 'init-cnfonts)
